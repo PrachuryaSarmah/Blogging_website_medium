8af318504c663edb35185fe09243535550db4247
Prachurya Sarmah <prachuryasarmah@Prachuryas-MacBook-Air.local>
2023-08-04 14:16:15 +0530

    Updated Filter

--- src/components/PostFilter.js
+++ src/components/PostFilter.js
@@ -1,19 +1,74 @@
-import React from 'react';
+// import React from 'react';
+
+// const PostFilter = ({ handleFilterChange }) => {
+//   return (
+//     <div>
+//       <h2>Filter Posts</h2>
+//       <label>
+//         Filter by:
+//         <select onChange={handleFilterChange}>
+//           <option value="all">All</option>
+//           <option value="author">Author</option>
+//           <option value="date">Date</option>
+//           <option value="likes">Likes</option>
+//           <option value="comments">Comments</option>
+//         </select>
+//       </label>
+//     </div>
+//   );
+// };
+
+// export default PostFilter;
+
+
+import React, { useState } from 'react';
 
 const PostFilter = ({ handleFilterChange }) => {
+  const [minLikes, setMinLikes] = useState('');
+  const [minComments, setMinComments] = useState('');
+  const [authorName, setAuthorName] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleLikesChange = (e) => {
+    setMinLikes(e.target.value);
+  };
+
+  const handleCommentsChange = (e) => {
+    setMinComments(e.target.value);
+  };
+
+  const handleAuthorChange = (e) => {
+    setAuthorName(e.target.value);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleFilter = () => {
+    handleFilterChange(minLikes, minComments, authorName, date);
+  };
+
   return (
     <div>
       <h2>Filter Posts</h2>
       <label>
-        Filter by:
-        <select onChange={handleFilterChange}>
-          <option value="all">All</option>
-          <option value="author">Author</option>
-          <option value="date">Date</option>
-          <option value="likes">Likes</option>
-          <option value="comments">Comments</option>
-        </select>
+        Minimum Likes:
+        <input type="number" value={minLikes} onChange={handleLikesChange} />
       </label>
+      <label>
+        Minimum Comments:
+        <input type="number" value={minComments} onChange={handleCommentsChange} />
+      </label>
+      <label>
+        Author:
+        <input type="text" value={authorName} onChange={handleAuthorChange} />
+      </label>
+      <label>
+        Date:
+        <input type="date" value={date} onChange={handleDateChange} />
+      </label>
+      <button onClick={handleFilter}>Apply Filter</button>
     </div>
   );
 };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,3 +1,66 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import PostFilter from './PostFilter';
+
+// const PostList = () => {
+//   const [posts, setPosts] = useState([]);
+//   const [filteredPosts, setFilteredPosts] = useState([]);
+//   const [filter, setFilter] = useState('all');
+
+//   useEffect(() => {
+//     fetchPosts();
+//   }, []);
+
+//   const fetchPosts = async () => {
+//     try {
+//       const response = await axios.get('YOUR_ROR_API_ENDPOINT/posts');
+//       setPosts(response.data);
+//       setFilteredPosts(response.data);
+//     } catch (error) {
+//       console.error('Error fetching posts:', error);
+//     }
+//   };
+
+//   const handleFilterChange = (e) => {
+//     const value = e.target.value;
+//     setFilter(value);
+
+//     if (value === 'all') {
+//       setFilteredPosts(posts);
+//     } else if (value === 'author') {
+//       const sortedByAuthor = [...posts].sort((a, b) => a.author.localeCompare(b.author));
+//       setFilteredPosts(sortedByAuthor);
+//     } else if (value === 'date') {
+//       const sortedByDate = [...posts].sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime));
+//       setFilteredPosts(sortedByDate);
+//     } else if (value === 'likes') {
+//       const sortedByLikes = [...posts].sort((a, b) => b.likes - a.likes);
+//       setFilteredPosts(sortedByLikes);
+//     } else if (value === 'comments') {
+//       const sortedByComments = [...posts].sort((a, b) => b.comments - a.comments);
+//       setFilteredPosts(sortedByComments);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>Post List</h2>
+//       <PostFilter handleFilterChange={handleFilterChange} />
+//       {filteredPosts.map((post) => (
+//         <div key={post.id}>
+//           <h3>{post.title}</h3>
+//           <p>Author: {post.author}</p>
+//           <p>Date: {post.dateTime}</p>
+//           <p>Date: {post.topic}</p>
+//           {/* Display other post details here */}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// // export default PostList;
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PostFilter from './PostFilter';
@@ -21,25 +84,34 @@ const PostList = () => {
     }
   };
 
-  const handleFilterChange = (e) => {
-    const value = e.target.value;
-    setFilter(value);
+  const handleFilterChange = (minLikes, minComments, authorName, selectedDate) => {
+    setFilter('custom'); // Set a custom filter identifier, if desired.
 
-    if (value === 'all') {
-      setFilteredPosts(posts);
-    } else if (value === 'author') {
-      const sortedByAuthor = [...posts].sort((a, b) => a.author.localeCompare(b.author));
-      setFilteredPosts(sortedByAuthor);
-    } else if (value === 'date') {
-      const sortedByDate = [...posts].sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime));
-      setFilteredPosts(sortedByDate);
-    } else if (value === 'likes') {
-      const sortedByLikes = [...posts].sort((a, b) => b.likes - a.likes);
-      setFilteredPosts(sortedByLikes);
-    } else if (value === 'comments') {
-      const sortedByComments = [...posts].sort((a, b) => b.comments - a.comments);
-      setFilteredPosts(sortedByComments);
+    let filteredByLikesAndComments = [...posts];
+
+    // Filter by Likes
+    if (filter === 'likes') {
+      filteredByLikesAndComments = filteredByLikesAndComments.filter((post) => post.likes >= minLikes);
     }
+
+    // Filter by Comments
+    if (filter === 'comments') {
+      filteredByLikesAndComments = filteredByLikesAndComments.filter((post) => post.comments >= minComments);
+    }
+
+    // Filter by Author
+    if (filter === 'author') {
+      filteredByLikesAndComments = filteredByLikesAndComments.filter((post) => post.author === authorName);
+    }
+
+    // Filter by Date
+    if (filter === 'date') {
+      filteredByLikesAndComments = filteredByLikesAndComments.filter(
+        (post) => new Date(post.dateTime) >= new Date(selectedDate)
+      );
+    }
+
+    setFilteredPosts(filteredByLikesAndComments);
   };
 
   return (
@@ -51,7 +123,8 @@ const PostList = () => {
           <h3>{post.title}</h3>
           <p>Author: {post.author}</p>
           <p>Date: {post.dateTime}</p>
-          <p>Date: {post.topic}</p>
+          <p>Likes: {post.likes}</p>
+          <p>Comments: {post.comments}</p>
           {/* Display other post details here */}
         </div>
       ))}
@@ -60,3 +133,5 @@ const PostList = () => {
 };
 
 export default PostList;
+
+
